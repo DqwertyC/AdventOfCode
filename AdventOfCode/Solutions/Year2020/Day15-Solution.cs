9c076b3e60c0f6ee5bbedd17e00fde400343104c
DqwertyC <53627062+DqwertyC@users.noreply.github.com>
2020-12-14 22:03:04 -0800

    2020 Day 15
    This may or may not run as is, can't remote into my normal computer to test it.

--- AdventOfCode/Solutions/Year2020/Day15-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day15-Solution.cs
@@ -7,20 +7,48 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day15 : ASolution
     {
-
+        private static int partOne;
+        private static int partTwo;
         public Day15() : base(15, 2020, "")
         {
+            int[] startVals = Input.ToIntArray(",");
+            Dictionary<int, int> lastAddress = new Dictionary<int, int>();
 
+            int nextVal;
+            int lastVal = 0;
+            int index = 0;
+
+            for (int i = 0; i < startVals.Length - 1; i++)
+            {
+                lastAddress[startVals[i]] = i;
+                index++;
+            }
+
+            nextVal = startVals[startVals.Length - 1];
+
+            while (index < 30000000)
+            {
+                lastVal = nextVal;
+
+                if ((!lastAddress.TryGetValue(nextVal, out int lastMatch))
+                    lastMatch = index;
+                nextVal = index - lastMatch;
+                lastAddress[lastVal] = index++;
+
+                if (index == 2020) partOne = lastVal;
+            }
+
+            partTwo = lastVal;
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return partOne.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return partTwo.ToString();
         }
     }
 }
